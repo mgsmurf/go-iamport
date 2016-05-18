872160cca1e46500591dc73da2cf387136237d0b
Mingi Kim <mgsmurf@gmail.com>
2016-05-19 00:12:02 +0900

    NewClient
    클라이언트 만들기

--- iamport.go
+++ iamport.go
@@ -17,3 +17,16 @@ type accessToken struct {
 	Token   string
 	Expired time.Time
 }
+
+// NewClient 아임포트로 부터 부여받은 API Key와 Api Secret을 사용하여 클라이언트를 새로 만든다.
+func NewClient(apiKey string, apiSecret string, cli *http.Client) *Client {
+	if cli == nil {
+		cli = &http.Client{}
+	}
+
+	return &Client{
+		APIKey:    apiKey,
+		APISecret: apiSecret,
+		HTTP:      cli,
+	}
+}
