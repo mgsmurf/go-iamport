6cd55734d006ac7eea0cedbd3ac6d3397212af00
Mingi Kim <mgsmurf@gmail.com>
2016-05-19 00:11:33 +0900

    Client 모델

--- iamport.go
+++ iamport.go
@@ -1,1 +1,19 @@
 package iamport
+
+import (
+	"net/http"
+	"time"
+)
+
+// Client 아임포트 서버와 통신하는 클라이언트
+type Client struct {
+	APIKey      string
+	APISecret   string
+	AccessToken accessToken
+	HTTP        *http.Client
+}
+
+type accessToken struct {
+	Token   string
+	Expired time.Time
+}
